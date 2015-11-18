4c45e2f866b271f4bf2fe320ef6bbead07c30631
Andrew Butterfield <andrew.butterfield@scss.tcd.ie>
2015-11-18 11:58:21 +0000

    tests now setup as per Lab01 rubric

--- Lab01/test/Test.hs
+++ Lab01/test/Test.hs
@@ -35,7 +35,17 @@ createMap w h c =
 {- HUnit Tests -}
 
 
-test_movement =
+test_movement_up =
+  let theMap = "###" ++
+               "#.#" ++
+               "#.#" ++
+               "###"
+      theScene = (Scene (createMap 3 4 theMap) (Player (1,2)) [])
+      expectedScene = (Scene (createMap 3 4 theMap) (Player (1,1)) [])
+  in
+    expectedScene @=? (handleInput 'i' theScene)
+
+test_movement_down =
   let theMap = "###" ++
                "#.#" ++
                "#.#" ++
@@ -44,6 +54,25 @@ test_movement =
       expectedScene = (Scene (createMap 3 4 theMap) (Player (1,2)) [])
   in
     expectedScene @=? (handleInput 'k' theScene)
+
+test_movement_left =
+  let theMap = "####" ++
+               "#..#" ++
+               "####"
+      theScene = (Scene (createMap 4 3 theMap) (Player (2,1)) [])
+      expectedScene = (Scene (createMap 3 4 theMap) (Player (1,1)) [])
+  in
+    expectedScene @=? (handleInput 'j' theScene)
+
+test_movement_right =
+  let theMap = "####" ++
+               "#..#" ++
+               "####"
+      theScene = (Scene (createMap 4 3 theMap) (Player (1,1)) [])
+      expectedScene = (Scene (createMap 3 4 theMap) (Player (2,1)) [])
+  in
+    expectedScene @=? (handleInput 'l' theScene)
+    
 
 {- QuickCheck Tests -}
 
@@ -58,17 +87,32 @@ prop_dropsome_drop n =
  n >= 0 ==>
   let xs = [0..9]
   in dropsome n xs == drop n xs
+  
+prop_distance :: Int -> Int -> Int -> Int -> Bool
+prop_distance x1 y1 x2 y2
+ = distance (x1,y1) (x2,y2) == dist1 (x1,y1) (x2,y2)
+   ||
+   distance (x1,y1) (x2,y2) == dist2 (x1,y1) (x2,y2)
+ where
+   dist1 (x1,y1) (x2,y2) = abs(x1-x2)+abs(y1-y2)
+   dist2 (x1,y1) (x2,y2)
+     = round $ sqrt $ fromIntegral ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
 
 main = do { putStrLn "Test.main..." ; defaultMain tests }
 
 tests :: [TF.Test]
 tests = [
-          testGroup "Test Collisions" [
-            testCase "Player Movement [0 marks]" test_movement
-          ],
-
-          testGroup "takesome and dropsome" [
-            testProperty "takesome works like Prelude.take" prop_takesome_take,
-            testProperty "dropsome works like Prelude.drop" prop_dropsome_drop
+          testGroup "Movement [1 mark]" [
+            testCase "Move Up"    test_movement_up
+          , testCase "Move Down"  test_movement_down
+          , testCase "Move Left"  test_movement_left
+          , testCase "Move Right" test_movement_right
+          ]
+        , testGroup "takesome and dropsome [4 marks]" [
+            testProperty "takesome works like Prelude.take [2 marks]" prop_takesome_take
+          , testProperty "dropsome works like Prelude.drop [2 marks]" prop_dropsome_drop
+          ]
+        , testGroup "distance [5 marks]" [
+            testProperty "distance is Manhattan or Euclidean" prop_distance
           ]
         ]
